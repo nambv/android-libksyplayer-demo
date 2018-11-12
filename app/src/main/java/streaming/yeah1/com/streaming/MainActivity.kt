8fcaab186ab0149050ce951bd15a2b7e7f6b8d0c
nambv <nambuivu@gmail.com>
2018-11-12 14:18:59 +0700

    Init project

---
+++ app/src/main/java/streaming/yeah1/com/streaming/MainActivity.kt
@@ -0,0 +1,12 @@
+package streaming.yeah1.com.streaming
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
